4af2ff0ae0f8c7eb7000249e0d5d0b0ddb8fa30d
Volodymyr Voznenko <voznenko7@gmail.com>
2025-08-28 22:08:49 +0200

    Kleine Änderung

--- backend/src/main/java/group/backend/auth/LoginGeraetRepo.java
+++ backend/src/main/java/group/backend/auth/LoginGeraetRepo.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginGeraetRepo extends JpaRepository<LoginGeraet, Long> {
     boolean existsByBenutzerIdAndGeraeteHash(Long benutzerId, String geraeteHash);
+    long countByBenutzer_Id(Long benutzerId);
 }
